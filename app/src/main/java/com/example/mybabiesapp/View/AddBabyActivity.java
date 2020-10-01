87e1380a95367c9bdb34ca9fd8f3b24f147341ba
resitdemir <resitdemir747@gmail.com>
2020-10-01 19:53:06 +0300

    myCommit

--- app/src/main/java/com/example/mybabiesapp/View/AddBabyActivity.java
+++ app/src/main/java/com/example/mybabiesapp/View/AddBabyActivity.java
@@ -5,17 +5,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mybabiesapp.Helper.DataConverter;
@@ -25,16 +30,22 @@ import com.example.mybabiesapp.R;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Calendar;
 
 public class AddBabyActivity extends AppCompatActivity {
-    private EditText etAd,etdogumTarihi,etcinsiet,etgebelikYasi,etiliskiler;
+    private EditText etAd;
+    private String etdogumTarihi;
+    private EditText etcinsiet;
+    private EditText etgebelikYasi;
+    private EditText etiliskiler;
     private EditText etagirlik;
     private EditText etuzunluk;
     private EditText etkafaUzunluk;
-    private Button btniptal,btnkaydet,btnPick;
+    private Button btniptal,btnkaydet,btnPick,btnSelectDate;
     private ImageView imageView;
     Bitmap bmpImage;
-    Uri imageData;
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     private static final int GALLERY_REQUEST_CODE = 123;
     private static final int PERMISSION_REQUEST_CODE = 200;
 
@@ -80,7 +91,7 @@ public class AddBabyActivity extends AppCompatActivity {
 
             private void saveNote() {
                 String ad = etAd.getText().toString();
-                String dogumTarihi = etdogumTarihi.getText().toString();
+                String dogumTarihi = etdogumTarihi;
                 String cinsiyet = etcinsiet.getText().toString();
                 String gebelikYasi =etgebelikYasi.getText().toString();
                 String agirlik =etagirlik.getText().toString();
@@ -107,6 +118,31 @@ public class AddBabyActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(AddBabyActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                String date = month + "/" + dayOfMonth + "/" +year;
+                etdogumTarihi = date;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 
     private void pickImageFromGallery(){
@@ -151,7 +187,8 @@ public class AddBabyActivity extends AppCompatActivity {
 
     public void init(){
         etAd = findViewById(R.id.txtad);
-        etdogumTarihi = findViewById(R.id.txtdogumTarihi);
+        btnSelectDate = findViewById(R.id.selectDate);
+        mDisplayDate = findViewById(R.id.dogumTarihi);
         etcinsiet = findViewById(R.id.cinsiet);
         etgebelikYasi = findViewById(R.id.gebelikYasi);
         btniptal = findViewById(R.id.txtiptal);
